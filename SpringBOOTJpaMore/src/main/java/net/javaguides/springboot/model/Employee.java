13e87f9feb9dc93ec1e570932f2aa0a417a0a505
Gleb Son <songleb81@gmail.com>
2022-06-17 15:27:46 +0300

    Update Employee.java

--- SpringBOOTJpaMore/src/main/java/net/javaguides/springboot/model/Employee.java
+++ SpringBOOTJpaMore/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,12 +1,14 @@
 package net.javaguides.springboot.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -23,28 +25,4 @@ public class Employee {
 	
 	@Column(name = "email")
 	private String email;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
