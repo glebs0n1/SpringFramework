947281f7d8525501a314ba99a3aefac584d6b2b8
Gleb Son <songleb81@gmail.com>
2022-06-17 19:36:55 +0300

    Update SpringbootThymeleafCrudWebAppApplication.java

--- SpringBOOTJpaMore/src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
+++ SpringBOOTJpaMore/src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootThymeleafCrudWebAppApplication {
+public class SpringbootJpa {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootThymeleafCrudWebAppApplication.class, args);
+		SpringApplication.run(SpringbootJpa.class, args);
 	}
 
 }
